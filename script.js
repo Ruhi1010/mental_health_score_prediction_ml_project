ad8343a2b7d32e02c5ddfee82a0e3341651a9876
Ruhi1010 <rashedial52@gmail.com>
2026-09-25 04:38:54 +0600

    .

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 (() => {
   "use strict";
 
-  const API_BASE = "https://mental-health-score-prediction-ml-3c0b.onrender.com";
+  const API_BASE = "https://mental-health-score-prediction-ml-umah.onrender.com";
 
   const form = document.getElementById("predict-form");
   const submitBtn = document.getElementById("submit-btn");
